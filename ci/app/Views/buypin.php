045437be6bcd1b81b6bbbf6e4092adbe2717c422
jsdcommtech <69227912+jsdcommtech@users.noreply.github.com>
2022-11-18 23:10:26 +0100

    Update buypin.php

--- ci/app/Views/buypin.php
+++ ci/app/Views/buypin.php
@@ -1,7 +1,7 @@
 
 <div class="container px-md-5 mx-md-5">
         <h5 class=""><a href="<?=base_url('/')?>">Home</a> > Buy pin for registeration</h5>
-        <h4 class="lead">Make a transfer of #6780 to any of the bank account provided below and send your proof of payment to the contact to collect your pin for registeration</h4>
+        <h4 class="lead">Make a transfer of #3800 to any of the bank account provided below and send your proof of payment to the contact to collect your pin for registeration</h4>
         <div class="row">
             <div class="col-12 col-md-6 mb-3">
                 <div class="card">

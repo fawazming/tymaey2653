5b9698f7797b5244eec490487b274034ff4c9f8b
jsdcommtech <69227912+jsdcommtech@users.noreply.github.com>
2022-12-07 12:13:27 +0100

    Update header.php

--- ci/app/Views/header.php
+++ ci/app/Views/header.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="<?=base_url('assets/chota.css')?>">
-    <title>TYMa Registration Portal</title>
+    <title>TYMa & TYLF Registration Portal</title>
 </head>
 <body>
     <div class="pt-5 pb-2 bg-light shadow-lg mb-4 ">
@@ -14,6 +14,7 @@
             <div class="d-flex align-items-center ">
                <img src="<?=base_url('img/logo.png')?>" class="img-fluid" width="75px" alt="Giftful">
                <h2 class="h4 ml-5">The Young Muslims Association <br>Egba/Yewa Zone <br></h2>
+               <h2 class="h4 ml-5">Treasure Young Leaders Foundation <br>Egba/Yewa Zone <br></h2> 
             </div>
         </div>
     </div>
